bacc8a8467015e070d6b3a184b846badba86df2a
Abdikhadir <Abdulkadir.muhiyadin@student.moringaschool.com>
2021-09-04 18:23:24 +0300

    contains script.js,index.html README.md and css

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let add = function(number1, number2) {
+    return number1 + number2;
+  };
+  let number1 = parseInt(prompt("9:"));
+  let number2 = parseInt(prompt("4:"));
+  alert(add(number1, number2));
